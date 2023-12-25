98568203194e51de787ef03bf64dfca42e5bdf03
Dev Agrawal <agrawadv@mail.uc.edu>
2023-12-24 22:26:24 -0500

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="flex flex-col items-center p-16">
-          <h1 className="text-3xl mb-8">Serverlesspresso</h1>
+          <Link href="/">
+            <h1 className="text-3xl mb-8">Serverlesspresso</h1>
+          </Link>
           <div className="m-4">{children}</div>
         </main>
       </body>
